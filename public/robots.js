c79852def73874ba9015f06be093bbde583616aa
stayhappyandloved <theajxz14@gmail.com>
2020-09-28 21:33:29 +0800

    used a map(for loop recommended) inside CardList.js to loop inside the robots file and display all the robot cards.

---
+++ public/robots.js
@@ -0,0 +1,75 @@
+export const robots = [
+{
+	id: 1,
+	name: 'April Joy',
+	username: 'April',
+	email: 'sincere@april.biz'
+},
+{
+	id: 2,
+	name: 'Nina Eunice',
+	username: 'Nina',
+	email: 'pretty@nina.eu'
+},
+{
+	id: 3,
+	name: 'Roi Quadruple',
+	username: 'roilex',
+	email: 'roilex@gmail.com'
+},
+{
+	id: 4,
+	name: 'Ariana Mae',
+	username: 'Ari',
+	email: 'ariana@up.com'
+},
+{
+	id: 5,
+	name: 'Ahmed Alexandria',
+	username: 'ahmed',
+	email: 'ahmedcool@engineering.com'
+},
+{
+	id: 6,
+	name: 'Pamela Amor',
+	username: 'pame',
+	email: 'pameawesome@gmail.com'
+},
+{
+	id: 7,
+	name: 'Lolita Nanana',
+	username: 'lol',
+	email: 'lolita@gmail.com'
+},
+{
+	id: 8,
+	name: 'Dan Rogue',
+	username: 'dan',
+	email: 'rogue@gmail.com'
+},
+{
+	id: 9,
+	name: 'Luis Santos',
+	username: 'luis',
+	email: 'luissantos@gmail.com'
+},
+{
+	id: 10,
+	name: 'Juliana Merida',
+	username: 'Julie',
+	email: 'julie@gmail.com'
+},
+
+
+
+
+
+
+
+
+
+
+
+
+
+]
