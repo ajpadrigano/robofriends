6f13c231a1ce3361fb709ebf6b9315a3e2aaf92b
stayhappyandloved <theajxz14@gmail.com>
2020-09-28 20:42:51 +0800

    used const Hello instead of class to create and pass props to component

--- src/Hello.js
+++ src/Hello.js
@@ -2,15 +2,15 @@ import React, { Component } from 'react';
 import './Hello.css'
 
 
-class Hello extends Component{
-	render() {
-		return (
-			<div className='f1 tc'>
-			<h1>Hello World </h1>
-			<p> Welcome to React {this.props.greeting} </p>
-			</div>
-			);
-	}
+const Hello = (props) =>  {
+
+	return (
+		<div className='f1 tc'>
+		<h1>Hello World </h1>
+		<p> Welcome to React {props.greeting} </p>
+		</div>
+	);
+	
 }
 
 export default Hello;
