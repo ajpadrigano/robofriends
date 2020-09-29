0b6abdac457ea13aac578f08c78185d0a80c8b24
stayhappyandloved <theajxz14@gmail.com>
2020-09-29 15:03:53 +0800

    fixed minor issues

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
+import ErrorBoundary from '../components/ErrorBoundary';
 import './App.css';
 
 
@@ -41,7 +42,9 @@ class App extends Component{
 				<h1 className='f2'>RoboFriends</h1>
 				<SearchBox searchChange={this.onSearchChange}/>
 				<Scroll>
-					<CardList robots={filteredRobots}/>
+					<ErrorBoundary>
+						<CardList robots={filteredRobots}/>
+					</ErrorBoundary>
 				</Scroll>		
 			</div>
 	}
