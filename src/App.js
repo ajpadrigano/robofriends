8c4ab0e05e21a337c0a66b746577f7594f922d65
stayhappyandloved <theajxz14@gmail.com>
2020-09-29 13:28:08 +0800

    added an if condition when the fetched data is empty

--- src/App.js
+++ src/App.js
@@ -31,13 +31,17 @@ class App extends Component{
 			return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
 
 		})
-		return (
-		<div className='tc'>
-			<h1 className='f2'>RoboFriends</h1>
-			<SearchBox searchChange={this.onSearchChange}/>
-			<CardList robots={filteredRobots}/>
-		</div>
-	);	
+		if(this.state.robots.length === 0 ){
+			return <h1> Loading </h1>
+		}else{
+			return (
+				<div className='tc'>
+					<h1 className='f2'>RoboFriends</h1>
+					<SearchBox searchChange={this.onSearchChange}/>
+					<CardList robots={filteredRobots}/>
+				</div>
+			);	
+		}	
 	}
 	
 	
