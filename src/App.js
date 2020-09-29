7caebeee943e8dabcea1a1f05e5d1ef5f6bf46ec
stayhappyandloved <theajxz14@gmail.com>
2020-09-29 12:53:38 +0800

    implemented the searchbox functionality

--- src/App.js
+++ src/App.js
@@ -12,16 +12,21 @@ class App extends Component{
 	        searchfield: ''
 		}
 	}
-
-	onSearchChange(event){
-		console.log(event.target.value);
+	//for your own methods
+	onSearchChange = (event) =>{
+		this.setState({searchfield:event.target.value});
+		
 	}
 	render(){
+		const filteredRobots = this.state.robots.filter(robots =>{
+			return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+
+		})
 		return (
 		<div className='tc'>
 			<h1>RoboFriends</h1>
 			<SearchBox searchChange={this.onSearchChange}/>
-			<CardList robots={this.state.robots}/>
+			<CardList robots={filteredRobots}/>
 		</div>
 	);	
 	}
