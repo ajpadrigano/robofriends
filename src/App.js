5380734324fd1cfbabc263c7d8205a022c3d0c8e
stayhappyandloved <theajxz14@gmail.com>
2020-09-29 13:24:34 +0800

    implemented functionality of fetching data from external source

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import CardList from './CardList';
-import {robots} from './robots';
 import SearchBox from './SearchBox';
 import './App.css';
 
@@ -8,9 +7,19 @@ class App extends Component{
 	constructor(){
 		super()
 		this.state = {
-			robots: robots,
+			robots: [],
 	        searchfield: ''
 		}
+	}
+
+	componentDidMount(){
+		fetch('https://jsonplaceholder.typicode.com/users').then(response=>{
+			return response.json()
+
+		}).then(users =>{
+			this.setState({robots:users})
+		});
+		
 	}
 	//for your own methods
 	onSearchChange = (event) =>{
